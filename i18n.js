bdf9ae62cc1733d8307bcdef0a525f9b21cd3214
aasem shoshari <117245999+aasemShoshari1998@users.noreply.github.com>
2024-02-13 18:56:50 +0300

    Add files via upload

---
+++ i18n.js
@@ -0,0 +1,282 @@
+export const messages = {
+    en: {
+        reviews: "Reviews",
+        yourReview: "Your review",
+        aboutUs:
+            "Hospitality Eagles?? Specialists in hospitality supplies, hotel supplies, and hotel equipments, the importance of the role we play in achieving a distinctive experience for guests. We at Nosour Al-Diyafa Foundation aim to provide a wide range of products and services that ensure comfort and luxury in hotels. These products include skin care, body care, personal care and more services that meet your needs. Hotels can equip their accommodations with the best equipment and amenities to ensure an unforgettable experience for guests.",
+        aboutUs2:
+            "Nosour Al-Dhiyafah Establishment works in the field of hotel supplies and hotel equipments on many features for clients. These features include fast and reliable delivery, ensuring product quality, providing diverse and customized options to meet customers' needs, in addition, special price offers and discounts are sent to special customers on a weekly basis. To give the customer the ability to compare prices, whichever is lower, so they can make decisions more accurately and appropriately.",
+        aboutUs3:
+            "We aim to satisfy customers, enhance their satisfaction and personal experience, and work in a more forward-looking and visionary reality in terms of options and achieving the best together.",
+        header: {
+            home: "Home",
+            shop: "Shop",
+            contact: "Contact us",
+            about: "About us",
+            login: "Login",
+            register: "Register",
+            company: "Eagles Hospitality",
+            companyCompliment: "Hospitality supplies and hotel equipments",
+        },
+        rateOurProduct: "Rate our product",
+
+        comment: "Comment",
+        shopNow: {
+            title: "SHOP NOW",
+        },
+        securePayment: {
+            title: "Security Payment",
+            explaination: "100% Security Payment",
+        },
+        dayReturn: {
+            title: "10 Day Return",
+            explaination: "10 day money guarantee",
+        },
+        support: {
+            title: "24/7 Support",
+            explaination: "Support every time fast",
+        },
+        products: {
+            title: "Products",
+            deleteProductSure:
+                "Are you sure you want to delete the selected product?",
+            deleteProductsSure:
+                "Are you sure you want to delete the selected products?",
+            update: "Update",
+            bestSelling: "BESTSELLER",
+            size: "Size :",
+            quantity: "Quantity per Packet :",
+            category1: "Skin care",
+            category2: "Personal care",
+            category3: "Body care",
+            category4: "Extra care",
+            shopCategory: "Categories",
+        },
+        bestSellingProducts: {
+            title: "Bestseller Products",
+        },
+        satisfiedCustomers: {
+            title: "satisfied customers",
+        },
+        qualityOfService: {
+            title: "Quality of Service",
+        },
+        qualityCertificates: {
+            title: "quality certificates",
+        },
+        availableProducts: {
+            title: "Available Products",
+        },
+        footer: {
+            address: "Address : Saudi Arabia - Jeddah",
+            email: "Email :",
+            PaymentAccepted: "Payment Accepted",
+            contact: "Contact us",
+            account: "Account",
+            shopInfo: "Shop Info",
+            readMore: "Read More",
+            copyRights: "All rights reserved.",
+            callUs: "Call us :",
+        },
+        profile: {
+            title: "Profile",
+            profileInfo: "Profile Information",
+            profileInfoParagraph:
+                "Update your account's profile information and email address.",
+            profileInfoName: "Name",
+            profileInfoEmail: "Email",
+            updatePassword: "Update Password",
+            updatePasswordParagraph:
+                "Ensure your account is using a long, random password to stay secure.",
+            updatePasswordCurrent: "Current Password",
+            updatePasswordNew: "New Password",
+            updatePasswordConfirm: "Confirm Password",
+            deleteAccount: "Delete Account",
+            deleteAccountConfirmTitle:
+                "Are you sure you want to delete your account?",
+            deleteAccountConfirmParagraph1:
+                "Once your account is deleted, all of its resources and data will be permanently deleted.",
+            deleteAccountConfirmParagraph2:
+                "Please enter your password to confirm you would like to permanently delete your account.",
+            password: "password",
+            cancel: "cancel",
+            save: "Save",
+            saved: "Saved",
+            profileImage: "Profile Image",
+            alreadyRegistered: "Already registered ?",
+            rememberMe: "Remember me",
+        },
+        logout: {
+            title: "Logout",
+        },
+        contact: {
+            title: "Address",
+            address: "Saudi Arabia - Jeddah",
+            mailUs: "Mail Us",
+            gmail: "zaidghzawi118{'@'}gmail.com",
+            telephoneTitle: "Telephone",
+            telephone: "+966540349846",
+
+            formName: "Enter your Name",
+            formEmail: "Enter your Email",
+            formMessage: "Your Message",
+            formSubmit: "Send",
+        },
+        createProduct: {
+            category: "Category",
+            nameEnglish: "Name English",
+            nameArabic: "Name Arabic",
+            descEnglish: "Description English",
+            descArabic: "Description Arabic",
+            size: "Size",
+            QuantityPerPacket: "Quantity per Packet",
+            bestSelling: "Best Selling",
+            bestSellingOptions: {
+                yes: "Yes",
+                no: "No",
+            },
+            create: "Create",
+            cancel: "Cancel",
+        },
+        productRate: "Product rate :",
+    },
+    ar: {
+        yourReview: "قيّم هذا المنتج",
+        reviews: "التقييم",
+        productRate: "تقييم المنتج",
+        rateOurProduct: "قيّم هذا المنتج",
+
+        aboutUs:
+            ".نســـــــــــور الضيافة ؟؟ مختصين في مستلزمات الضيافة والامدادات الفندقية وتجهيزات الفنادق , مدى أهمية الدور اللذي تلعبه دورًا حاسمًا في تحقيق تجربة مميزة للنزلاء.   نهدف نحن في مؤسسة نسور الضيافة   إلى توفير مجموعة واسعة من المنتجات والخدمات التي تضمن الراحة والفخامة في الفنادق. تشمل هذه المنتجات ،مستلزمات العناية بالبشرة والعناية بالجسم , والعناية الشخصية والمزيد المزيد من الخدمات التي تلبي احتياجاتكم يمكن للفنادق تجهيز أماكن الإقامة بأفضل التجهيزات  والأدوات لضمان تجربة لا تُنسى للنزلاء",
+        aboutUs2:
+            ".تعمل مؤسسة نسور الضيافة في مجال الإمدادات الفندقية وتجهيزات الفنادق على  العديد من الامتيازات للعملاء.  تشمل هذه الامتيازات خدمة توصيل سريعة وموثوقة، ضمان جودة المنتجات، توفير خيارات متنوعة ومخصصة لتلبية احتياجات العملاء، وخدمة عملاء متفهمة ومتعاونة. بالإضافة إلى ذلك، يتم ارسال عروض الأسعار  والخصومات الخاصة للعملاء المميزين اسبوعيا بشكل مستمر . لاعطاء العميل امكانية المفاضلة في الأسعار أيهما أقل ويتمكن من اتخاذ القرارات بشكل أدق ومناسب",
+        aboutUs3:
+            ".نهدف إلى إرضاء العملاء وتعزيز رضاهم وتجربتهم الشخصية والعمل في واقع أكثر تطلعا ورؤية من حيث الخيارات والإرتقاء للأفضل سويآ",
+        shopNow: {
+            title: "تسوق الآن",
+        },
+        comment: "تعليق",
+
+        securePayment: {
+            title: "دفع آمن",
+            explaination: "دفع آمن %100",
+        },
+        dayReturn: {
+            title: "ضمان 10 أيام",
+            explaination: "ضمان استرجاع المال لمدة 10 أيام",
+        },
+        support: {
+            title: "دعم 24/7",
+            explaination: "دعم لا محدود واستجابة سريعة",
+        },
+        products: {
+            title: "المنتجات",
+            deleteProductSure: "هل أنت متأكد من أنك تريد حذف هذا المنتج؟",
+            deleteProductsSure: "هل أنت متأكد من أنك تريد حذف هذا المنتجات",
+            update: "تحديث",
+            bestSelling: "من الأفضل مبيعاً",
+            size: "الحجم :",
+            quantity: "الكمية بالكرتونة :",
+            category1: "العناية بالبشرة",
+            category2: "العناية الشخصية",
+            category3: "العناية بالجسم",
+            category4: "العناية الإضافية",
+            shopCategory: "الأصناف",
+        },
+        bestSellingProducts: {
+            title: "أفضل المنتجات مبيعا",
+        },
+        satisfiedCustomers: {
+            title: "زبائن يشعرون بالرضى",
+        },
+        qualityOfService: {
+            title: "ضمان الجودة",
+        },
+        qualityCertificates: {
+            title: "شهادات الجودة",
+        },
+        availableProducts: {
+            title: "المنتجات المتوفرة",
+        },
+        header: {
+            home: "الصفحة الرئيسية",
+            shop: "المتجر",
+            contact: "تواصل معنا",
+            about: "نبذة عنا",
+            login: "تسجيل الدخول",
+            register: "أنشئ حساب",
+            company: "مؤسسة نسور الضيافة",
+            companyCompliment: "مستلزمات الضيافة والتجهيزات الفندقية",
+        },
+        footer: {
+            address: "العنوان : المملكة العربية السعودية - جدة",
+            email: " البريد الإلكتروني :",
+            PaymentAccepted: "طرق الدفع المتاحة :",
+            contact: "تواصل معنا",
+            account: "معلومات الحساب",
+            shopInfo: "التسوق الإلكتروني",
+            readMore: "إقرأ المزيد",
+            copyRights: "جميع الحقوق محفوظة",
+            callUs: "إتصل بنا :",
+        },
+        profile: {
+            title: "حسابك الشخصي",
+            profileInfo: "معلومات الحساب",
+            profileInfoParagraph:
+                "تغيير معلومات الحساب الشخصي والبريد الإلكتروني",
+            profileInfoName: "الإسم",
+            profileInfoEmail: "البريد الإلكتروني",
+            updatePassword: "تغيير كلمة المرور",
+            updatePasswordParagraph:
+                "يرجى إدخال كلمة مرور قوية ومكونة من أحرف وأرقام ",
+            updatePasswordCurrent: "كلمة المرور الحالية",
+            updatePasswordNew: "كلمة المرور الجديدة",
+            updatePasswordConfirm: "تأكيد كلمة المرور",
+            deleteAccount: "حذف الحساب",
+            deleteAccountConfirmTitle:
+                "هل أنت متأكد أنك تريد حذف حسابك الشخصي ؟",
+            deleteAccountConfirmParagraph1:
+                ".بمجرد حذف حسابك الشخصي، سوف يتم حذف جميع المصادر والمعلومات المتعلقة بحسابك",
+            deleteAccountConfirmParagraph2: "يرجى إدخال كلمة المرور للتأكيد ",
+            password: "كلمة المرور",
+            cancel: "تراجع",
+            save: "حفظ",
+            saved: "تم الحفظ",
+            profileImage: "صورة الملف الشخصي",
+            alreadyRegistered: "لديك حساب ؟",
+            rememberMe: "تذكرني",
+        },
+        logout: {
+            title: "تسجيل خروج",
+        },
+
+        contact: {
+            title: "العنوان",
+            address: "المملكة العربية السعودية - جدة",
+            mailUs: "تواصل معنا على البريد الإلكتروني",
+            gmail: "zaidghzawi118{'@'}gmail.com",
+            telephoneTitle: "الهاتف",
+            telephone: "+966540349846",
+            formName: "الإسم",
+            formEmail: "البريد الإلكتروني",
+            formMessage: "رسالتك",
+            formSubmit: "إرسال",
+        },
+        createProduct: {
+            category: "الصنف",
+            nameEnglish: "إسم إنجليزي",
+            nameArabic: "إسم عربي",
+            descEnglish: "وصف إنجليزي",
+            descArabic: "وصف عربي",
+            size: "الحجم",
+            QuantityPerPacket: "الكمية بالكرتونة",
+            bestSelling: "أفضل المنتجات مبيعا",
+            bestSellingOptions: {
+                yes: "نعم",
+                no: "لا",
+            },
+            create: "إنشاء",
+            cancel: "تراجع",
+        },
+    },
+};
